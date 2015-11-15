6e4fa1f0d273deaf76767e5a008bd50b7acdbbe9
Anna Pinson <apinson.txt@gmail.com>
2015-11-15 14:32:40 -0800

    Tried to get tilemap.scala to work

--- FreeTyle/src/freetyle/tilemap.scala
+++ FreeTyle/src/freetyle/tilemap.scala
@@ -1,22 +1,27 @@
-package freetyle
 
 import java.io.File
 import scala.util.parsing.combinator._
+import scala.io.Source
 import freetyle.parsing.parser
-import freetyle.semantics._
-
+import freetyle.semantics._ 
 
 /**
  * @author apinson
  */
 object tilemap {
-  val mapOpt = parameters.named get "map"
-  mapOpt match {
-    case (Some(mapFile)) =>
-      val mapCode = io.Source.fromFile(mapFile).mkString
-      parser(mapCode) match {
-        case parser.Success(ast, _) =>
-          loadAST(ast)
-      }
+  def main(args: Array[String]) = {
+    for (ln <- io.Source.stdin.getLines) println(ln)
+//    val mapOpt = args(0)
+//    mapOpt match {
+//      case mapFile =>{
+//        val mapCode = io.Source.fromFile(mapFile).mkString
+//        parser(mapCode) match {
+//          case parser.Success(ast, _) =>
+//            loadAST(ast)
+//          // If the parser fails, do error handling here
+//          case _ => null
+//        }
+//      }
+//    }
   }
 }
